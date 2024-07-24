bd1216315b66f2aa1fcae7eb1cbb646423984187
Bren Broussard <broussard.j.bren@gmail.com>
2024-07-24 16:39:52 -0500

    added theme.js using colortokens to set theme/color palette for light/dark mode, topography, and added code in App.js, state folder created and index.js for state, added code for index.js on client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
 
 function App() {
   return (
     <div className="app">
-      
+      <BrowserRouter>
+      <routes>
+        <route path="/" element={<LoginPage/>} />
+        <route path="/home" element={<HomePage/>} />
+        <route path="/profile/:userId" element={<ProfilePage/>} />
+      </routes>
+      </BrowserRouter>
     </div>
   );
 }
